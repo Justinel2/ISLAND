332f31c4e768d7c3d54225f615f911f0e63479c4
Nina Parenteau <n_par@cda119ev5635x.local>
2019-04-17 18:14:11 -0400

    added sine mvmt

---
+++ ClubMedz4/Assets/SineMouvement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineMouvement : MonoBehaviour
+{
+    GameObject water;
+    //Material m_Material;
+    //public int n;
+    public AnimationCurve myCurve;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(myCurve.Evaluate((Time.time % myCurve.length))*20, transform.position.y, myCurve.Evaluate((Time.time % myCurve.length / 2)) * 20);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Shark")
+        {
+
+        }
+    }
+}
+

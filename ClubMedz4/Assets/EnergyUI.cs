154773acd0c81ea21e82aaf8d07e2941cc8d5402
Justine Lardeux <j_lardeu@cda118ev5635x.local>
2019-04-17 15:48:48 -0400

    handle sleeping

--- ClubMedz4/Assets/EnergyUI.cs
+++ ClubMedz4/Assets/EnergyUI.cs
@@ -15,22 +15,24 @@ namespace TMPro.Examples
 
         private TMP_Text m_text;
 
-        private const string k_label = "Energy: ";
+        private const string k_label = "Energy: <#ff0000>{0}</color>";
+        private bool sleeping = false;
 
+        public static float energyLevel = 100.0f;
         public static float timer;
         public static bool timeStarted = false;
         public int minutes;
         private int seconds;
         private string time;
+        private float toFill;
 
         void Start()
         {
-
             // Get a reference to the TMP text component.
             m_text = GetComponent<TextMeshProUGUI>();
 
             // Set the size of the font.
-            m_text.fontSize = 18;
+            m_text.fontSize = 14;
 
             //Set the text
             m_text.text = "A <#0080ff>simple</color> line of text.";
@@ -46,16 +48,47 @@ namespace TMPro.Examples
 
         void Update()
         {
-            timer += Time.deltaTime;
-            minutes = Mathf.FloorToInt(timer / 60F);
-            seconds = Mathf.FloorToInt(timer - minutes * 60);
-            time = k_label + string.Format("{0:0}:{1:00}", minutes, seconds);
-           
+            GetTime();
+            sleeping = GameObject.Find("Player01").GetComponent<Sleep>().inHouse;
+            print(sleeping);
+
+            if (sleeping) {
+                HandleSleeping();
+            }
+            else
+                HandleDecay();
+
             if (!isStatic)
             {
                     // Set text
-                    m_text.SetText(time);
+                    m_text.SetText(k_label, (int)energyLevel);
             }
+        }
+
+        void GetTime()
+        {
+            timer += Time.deltaTime;
+            print(timer);
+            minutes = Mathf.FloorToInt(timer / 60F);
+            seconds = Mathf.FloorToInt(timer - minutes * 60);
+            //time = k_label + string.Format("{0:0}:{1:00}", minutes, seconds);
+        }
+
+        void HandleSleeping(){
+            toFill = 100.0f - energyLevel;
+            if (toFill > 0){
+                for (int i = 0; i <= toFill; i++){
+                    energyLevel++;
+                }
+            }
+            else
+                return;
+        }
+
+        void HandleDecay(){
+            if (seconds % 3 == 0 && seconds > 3)
+                energyLevel -= 0.03f;
+            return;
         }
     }
 }
---
+++ ClubMedz4/Assets/Sleep.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sleep : MonoBehaviour
+{
+    private GameObject house;
+    public bool inHouse = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        house = GameObject.Find("house");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPresence();
+    }
+
+    void CheckPresence() {
+        var dist = Vector3.Distance(transform.position, house.transform.position);
+        if (dist > 2 && dist < 9)
+        {
+            inHouse = true;
+        }
+        else
+            inHouse = false;
+    }
+}
+
